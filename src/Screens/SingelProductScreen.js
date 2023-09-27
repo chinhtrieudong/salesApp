c91684f32c6796b9e05e97521fe15468d69fca01
chinh <lebachinh2002@gmail.com>
2023-09-27 09:32:34 +0700

    SetUp sales-app

---
+++ src/Screens/SingelProductScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SingelProductScreen = () => {
+  return (
+    <View>
+      <Text>SingelProductScreen</Text>
+    </View>
+  )
+}
+
+export default SingelProductScreen
---
+++ src/color.js
@@ -0,0 +1,16 @@
+const color = {
+    main: "#48B600",
+    white: "#FFFFFF",
+    black: "#000000",
+    lightBlack: "BCBCBC",
+    orange: "#FFCE31",
+    red: "#FF0000",
+    gray: "#F4F4F4",
+    subGreen: "#EEFAE6",
+    deepGray: "#C4C4C4",
+    blue: "#3887F9",
+    underline: "#E5E5E5",
+    paypal: "3FFB730",
+}
+
+export default color;
