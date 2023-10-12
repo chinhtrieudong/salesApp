c8cb5186b496858c41e198d96cb33e462ae80a4d
chinh <lebachinh2002@gmail.com>
2023-10-12 10:28:36 +0700

    OrderInfo, OrderItem Components - PlaceOrderModel, PlaceOrderScreen Screens

--- App.js
+++ App.js
@@ -5,12 +5,13 @@ import CartScreen from './src/Screens/CartScreen';
 import ProfileScreen from './src/Screens/ProfileScreen';
 import ShippingScreen from './src/Screens/ShippingScreen';
 import PaymentScreen from './src/Screens/PaymentScreen';
+import PlaceOrderScreen from './src/Screens/PlaceOrderScreen';
 
 export default function App() {
     return (
         <NativeBaseProvider>
             <Box flex={1}>
-                <PaymentScreen />
+                <PlaceOrderScreen />
             </Box>
         </NativeBaseProvider>
     );
--- src/Screens/PlaceOrderScreen.js
+++ src/Screens/PlaceOrderScreen.js
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Box, Heading, ScrollView } from 'native-base';
+import React from 'react';
+import { FontAwesome, FontAwesome5, Ionicons } from '@expo/vector-icons';
+import Colors from '../Colors';
+import OrderInfo from '../Components/OrderInfo';
+import OrderItem from '../Components/OrderItem';
+import PlaceOrderModel from '../Components/PlaceOrderModel';
 
 const PlaceOrderScreen = () => {
-  return (
-    <View>
-      <Text>PlaceOrderScreen</Text>
-    </View>
-  )
-}
+    return (
+        <Box bg={Colors.subGreen} flex={1} safeArea pt={6}>
+            <Box>
+                <ScrollView
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <OrderInfo
+                        title="CUSTOMER"
+                        subTitle="Chinh"
+                        text="chinh@gmail.com"
+                        success
+                        icon={
+                            <FontAwesome
+                                name="user"
+                                size={30}
+                                color={Colors.white}
+                            />
+                        }
+                    />
+                    <OrderInfo
+                        title="SHIPPING INFO"
+                        subTitle="Shipping: Hackfb"
+                        text="Pay Method: Paypal"
+                        danger
+                        icon={
+                            <FontAwesome5
+                                name="shipping-fast"
+                                size={30}
+                                color={Colors.white}
+                            />
+                        }
+                    />
+                    <OrderInfo
+                        title="DELIVER TO"
+                        subTitle="Adress:"
+                        text="Japan, Osaka"
+                        danger
+                        icon={
+                            <Ionicons
+                                name="location-sharp"
+                                size={30}
+                                color={Colors.white}
+                            />
+                        }
+                    />
+                </ScrollView>
+            </Box>
+            {/* Order Item */}
+            <Box px={6} flex={1} pb={3}>
+                <Heading bold fontSize={15} isTruncated my={4}>
+                    PRODUCTS
+                </Heading>
+                <OrderItem />
+                {/* Total */}
+                <PlaceOrderModel />
+            </Box>
+        </Box>
+    );
+};
 
-export default PlaceOrderScreen
+export default PlaceOrderScreen;
